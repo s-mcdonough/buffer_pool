2e6ab4c3abb2a713540940fd1f37ba03888cf5c1
Sean McDonough <smcdono2@u.rochester.edu>
2021-01-09 22:46:58 -0700

    Adding robustness to mover object, to handle non-standard usage.

--- src/buffer_pool/buffer_pool.h
+++ src/buffer_pool/buffer_pool.h
@@ -2,25 +2,53 @@
 
 #include <deque>
 #include <memory>
+#include <mutex>
 
+/**
+ * @brief A 
+ * 
+ * @tparam T underlying type to manage
+ */
 template<typename T>
 class buffer_pool
 {
     using bp_type = buffer_pool<T>;
     using internal_pointer_type = std::unique_ptr<T>;
 
+    /**
+     * @brief Moves the object back into the buffer pool once it goes out of scope.
+     * 
+     * A custom deleter for the wrapped buffer object.
+     */
     struct mover
     {
         mover() = delete;
-        mover(bp_type* p_buffer_pool) : _p_buffer_pool(p_buffer_pool) {}
+        mover(bp_type* p_buffer_pool, T* ptr) 
+            : _p_buffer_pool(p_buffer_pool)
+            , _ptr(ptr) {}
 
         void operator()(T* p) const
         {
-            // We aren't deleting the object, just moving it
-            _p_buffer_pool->manage(p);
+            // Handle the case where the underlying pointer has been released
+            if (p)
+            {
+                // Handle the case where the initial pointer has been replaced
+                if (p == _ptr)
+                {
+                    // If it matches, we aren't deleting the object, but putting it back in the queue
+                    _p_buffer_pool->manage(p);
+                }
+                else
+                {
+                    // Otherwise, free the pointer
+                    delete p;
+                }
+                
+            }
         }
 
         bp_type* _p_buffer_pool;
+        T* _ptr;
     };
 
 public:
@@ -28,13 +56,45 @@ public:
     using pointer_type = std::unique_ptr<T, mover>;
     using size_type = std::size_t;
 
+    buffer_pool() = default;
+    ~buffer_pool() = default;
+    buffer_pool(const buffer_pool&) = delete;
+    buffer_pool& operator= (const buffer_pool&) = delete;
+
+    /**
+     * @brief Gets an buffer from the pool.
+     * 
+     * This decreases the number of available objets gettable from the pool. 
+     * 
+     * @return pointer_type A buffer for use. Returned to the pool once 
+     *                      the buffer goes out of scope.
+     */
     pointer_type get() 
     { 
-        auto ptr = pointer_type(_queue.front().release(), mover(this));
+        std::lock_guard lk(_mutex);
+        auto* raw_ptr = _queue.front().release();
+        auto ptr = pointer_type(raw_ptr, mover(this, raw_ptr));
         _queue.pop_front();
         return ptr;
     }
-    void manage(T* object) { _queue.push_back(internal_pointer_type(object)); }
+
+    /**
+     * @brief 
+     * 
+     * @param object 
+     */
+    void manage(T* object) 
+    { 
+        std::lock_guard lk(_mutex);
+        _queue.push_back(internal_pointer_type(object)); 
+    }
+
+    template<class... Args>
+    void emplace_manage(Args&&... args)
+    {
+        std::lock_guard lk(_mutex);
+        _queue.emplace_back(std::make_unique<T>(std::forward<Args>(args)...));
+    }
 
     size_type capacity() const noexcept { return _queue.max_size(); }
     size_type size() const noexcept { return _queue.size(); }
@@ -42,4 +102,5 @@ public:
 
 private:
     std::deque<internal_pointer_type> _queue;
+    std::mutex _mutex;
 };
--- test/unit/buffer_pool.cpp
+++ test/unit/buffer_pool.cpp
@@ -7,7 +7,7 @@ TEST_CASE("size operators", "[buffer_pool]")
     REQUIRE(bp.size() == 0);
     REQUIRE(bp.empty() == true);
 
-    SECTION("Add an element to the pool")
+    SECTION("Register an element with the pool")
     {
         const int value = 42;
         bp.manage(new int(value));
@@ -31,5 +31,27 @@ TEST_CASE("size operators", "[buffer_pool]")
         REQUIRE(bp.empty() == false);
     }
 
-    
+    SECTION("In place construct an element with the pool")
+    {
+        const int approx_pi = 3;
+        bp.emplace_manage(approx_pi);
+
+        // Size should be increased
+        REQUIRE(bp.size() == 1);
+        REQUIRE(bp.empty() == false);
+
+        // Test getting the value
+        {
+            auto ptr_to_val = bp.get();
+            CHECK(*ptr_to_val == approx_pi);
+
+            // The buffer is removed from the pool
+            REQUIRE(bp.size() == 0);
+            REQUIRE(bp.empty() == true);
+        }
+
+        // Size should be increased
+        REQUIRE(bp.size() == 1);
+        REQUIRE(bp.empty() == false);
+    }
 }
